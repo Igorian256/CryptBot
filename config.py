7c227e53b243766725dae9166f4540b61f9e84ec
Igor Kuznetsov <121581420+Igorian256@users.noreply.github.com>
2023-01-25 15:53:03 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 TOKEN = '5841232359:AAFLwTRRFMEYp2q__4_gFzHgjrxRtqpvYtE'
 keys = {
     'биткоин': 'BTC',
+    'Биткоин': 'BTC',
+    'биткойн': 'BTC',
+    'Биткойн': 'BTC',
     'эфириум': 'ETH',
     'доллар': 'USD',
+    'рубль': 'RUR',
     }
